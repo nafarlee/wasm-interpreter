b1eb73c51b45b148ffa2440c7abc4491a501856b
Nicholas Farley <nicholasafarley@gmail.com>
2022-12-18 23:58:31 -0500

    Read main.wasm file byte-by-byte

--- src/main.zig
+++ src/main.zig
@@ -1,1 +1,11 @@
-pub fn main() void {}
+const std = @import("std");
+
+pub fn main() !void {
+    const file = try std.fs.cwd().openFile("main.wasm", .{});
+    defer file.close();
+    const reader = file.reader();
+    while (true) {
+        const byte = reader.readByte() catch return;
+        std.debug.print("{x:0^2} ", .{byte});
+    }
+}
