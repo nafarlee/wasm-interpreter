607ff65d36654ab4bccde1e05ab66935859cadf3
Nicholas Farley <nicholasafarley@gmail.com>
2022-12-21 18:39:46 -0500

    Read entire file contents at once

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,11 @@
 const std = @import("std");
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
     const file = try std.fs.cwd().openFile("main.wasm", .{});
     defer file.close();
-    const reader = file.reader();
-    while (true) {
-        const byte = reader.readByte() catch return;
-        std.debug.print("{x:0^2} ", .{byte});
-    }
+    const contents = try file.readToEndAlloc(allocator, 2000);
+    defer allocator.free(contents);
+    std.debug.print("{s}", .{std.fmt.fmtSliceHexLower(contents)});
 }
