e3b32ab263f42970243f09b2fdba06ef9697d14e
Nicholas Farley <nicholasafarley@gmail.com>
2022-12-23 19:47:26 -0500

    Add step to run the built zig executable immediately

--- build.zig
+++ build.zig
@@ -5,4 +5,13 @@ pub fn build(b: *std.build.Builder) void {
     exe.setTarget(b.standardTargetOptions(.{}));
     exe.setBuildMode(b.standardReleaseOptions());
     exe.install();
+
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
