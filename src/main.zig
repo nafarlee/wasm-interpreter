59856d5f5e46b85511219b88b343eae39241788b
Nicholas Farley <nicholasafarley@gmail.com>
2023-01-16 16:52:03 -0500

    Add stub decode_preamble fn

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,17 @@
 const std = @import("std");
 
+pub fn decode_preamble(bytes: []u8, index: *usize) !void {
+    const magic = bytes[index.* .. index.* + 4];
+    for (magic) |byte| {
+        std.debug.print("0x{X:0^2} ", .{byte});
+    }
+    std.debug.print("\n", .{});
+    const version = bytes[index.* + 4 .. index.* + 8];
+    for (version) |byte| {
+        std.debug.print("0x{X:0^2} ", .{byte});
+    }
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
